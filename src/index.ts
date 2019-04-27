b3e3f47564d1eac18ebeae70212e56883790e1ab
Ulysse Carion <ulysse@segment.com>
2019-04-27 14:46:06 -0700

    Export useful stuff from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export { default as Schema } from "./Schema";
 export { default as CompiledSchema, compileSchema } from "./CompiledSchema";
+export { default as Registry } from "./Registry";
+export { default as Validator } from "./Validator";
+export { default as MaxDepthExceededError } from "./MaxDepthExceededError";
+export { default as NonRootError } from "./NonRootError";
+export { default as NoSuchDefinitionError } from "./NoSuchDefinitionError";
+export { default as NoSuchSchemaError } from "./NoSuchSchemaError";
+export { default as RegistryNotSealedError } from "./RegistryNotSealedError";
